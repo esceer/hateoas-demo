1fee39f11277e3fcebf5b6f212fd4eaa8f57ef91
zkutrol <roland.kutas@blackbelt.hu>
2019-11-12 15:35:57 +0100

    Updated traverson example to pagedModel based solution

--- traverson-client/src/main/java/com/example/demo/ClientApplication.java
+++ traverson-client/src/main/java/com/example/demo/ClientApplication.java
@@ -1,34 +1,26 @@
 package com.example.demo;
 
-import static java.util.Optional.ofNullable;
-
 import com.example.demo.model.Person;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Collection;
-import java.util.Collections;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.hateoas.client.Traverson;
 import org.springframework.hateoas.server.core.TypeReferences;
 
-@SpringBootApplication
 public class ClientApplication {
 
     public static void main(String[] args) throws URISyntaxException {
-        System.out.println(fetchPeople());
-        System.exit(0);
+        PagedModel<Person> peopleModel = fetchPeople();
+        System.out.println(peopleModel.getContent());
+        System.out.println(peopleModel.getMetadata());
     }
 
-    private static Collection<Person> fetchPeople() throws URISyntaxException {
+    private static PagedModel<Person> fetchPeople() throws URISyntaxException {
         Traverson traverson = new Traverson(new URI("http://localhost:8080/api/persons"), MediaTypes.HAL_JSON);
-        return ofNullable(
-            traverson
-                .follow("$._links.self.href")
-                .toObject(new TypeReferences.CollectionModelType<Person>() {
-                }))
-            .map(CollectionModel::getContent)
-            .orElse(Collections.emptyList());
+        return traverson
+            .follow("$._links.self.href")
+            .toObject(new TypeReferences.PagedModelType<Person>() {
+            });
     }
 }
