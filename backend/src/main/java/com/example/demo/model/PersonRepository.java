ca93d4b000a84a1c192247335ea69e1746535b5c
zkutrol <roland.kutas@blackbelt.hu>
2019-11-15 11:31:22 +0100

    Added basic pagin and custom query endpoints

--- backend/src/main/java/com/example/demo/model/PersonRepository.java
+++ backend/src/main/java/com/example/demo/model/PersonRepository.java
@@ -1,7 +1,15 @@
 package com.example.demo.model;
 
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
+    Page<Person> findByAgeAndEmail(Integer age, String email, Pageable pageable);
+
+    @Query("SELECT p FROM Person p where p.age = :age AND p.email LIKE %:email%")
+    List<Person> findByCustom(Integer age, String email);
 }
